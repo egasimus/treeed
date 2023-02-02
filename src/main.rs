3aa6fefe9ba4ab5a664f3c517d18cdf719f78650
Adam A <adam@hack.bg>
2023-02-02 17:58:23 +0200

    render stub tree with border

--- src/main.rs
+++ src/main.rs
@@ -18,25 +18,43 @@ struct App {
 }
 
 impl App {
+
     fn new () -> Self {
         Self { exited: &EXITED }
     }
+
     /// Set the exit flag, terminating the main loop before the next render.
     fn exit (&mut self) {
         self.exited.store(true, Ordering::Relaxed);
     }
+
     fn load (&mut self, path: &impl AsRef<std::path::Path>) {
     }
+
     fn save (&self, path: &impl AsRef<std::path::Path>) {
     }
+
 }
 
 impl Widget for App {
 
     impl_render!(self, out, area => {
-        Aligned(Align::Center, Border(InsetTall, Stacked::y(|add|{
-            //add(&self.devices);
-        }))).render(out, area)
+        Stacked::y(|add|{
+            add(Stacked::x(|add|{
+                add(&" C-l Load  ");
+                add(&"C-s Save  ");
+                add(&"Esc Toggle pane  ");
+            }));
+            add(Border(Flat, Inset, Stacked::y(|add| {
+                add(&"root     │");
+                add(&" ┝━foo   │");
+                add(&" ┝━bar   │");
+                add(&" │ ┝━foo │");
+                add(&" │ ┝━bar │");
+                add(&" │ ┕━baz │");
+                add(&" ┕━baz   │");
+            })));
+        }).render(out, area)
     });
 
     impl_handle!(self, event => {
